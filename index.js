fc97f4c6da65a16b2c9efcfb8c8a389412676ff1
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-10 12:59:16 +0200

    exercice filter

--- index.js
+++ index.js
@@ -387,3 +387,25 @@ console.log(movementsDescriptions); */
 	(mov, i, arr) => ` Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
 );
 console.log(movementsDescriptions); */
+
+//Filter
+
+const movements = [ 200, 450, -400, 3000, -650, -130, 70, 1300 ];
+
+/////////////////////////////////////////////////
+//USING FILTER only the positive value
+
+const deposits = movements.filter(function(mov) {
+	return mov > 0;
+});
+console.log(deposits);
+
+//second option
+/* const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor); */
+
+const withdrawls = movements.filter((mov) => mov < 0);
+console.log(withdrawls);
+
+//USING REDUCE
